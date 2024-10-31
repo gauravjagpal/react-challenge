575c36e11147674d4bc64315a7a1ca4223916d51
gaurav jagpal <gauravjagpal@hotmail.com>
2024-10-31 11:46:35 +0000

    test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
-import css from './App.module.css';
+
+import logo from './logo.svg';
+import './App.css';
+import StatefulGreeting from './components/StatefulGreeting';
 
 function App() {
   return (
-    <div className={css.App}>
-      {/* Add your components here */}
+    <div className="App">
+      <StatefulGreeting greeting="I'm a stateful class component!" name="Mike"/>
     </div>
   );
 }
